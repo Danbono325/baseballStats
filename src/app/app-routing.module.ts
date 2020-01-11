a19dd476efd0b05afc7c14893ede83c7959ac539
Daniel S. Boniface <s1087717@monmouth.edu>
2020-01-10 19:32:52 -0500

    fixed team stats page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { NavbarComponent } from "./components/navbar/navbar.component";
 import { LoginComponent } from "./components/login/login.component";
 import { TeamOverviewComponent } from "./components/team-overview/team-overview.component";
 import { TeamStatsComponent } from "./components/team-stats/team-stats.component";
@@ -11,8 +10,8 @@ const routes: Routes = [
   { path: "", component: LoginComponent },
   { path: "overview", component: TeamOverviewComponent },
   { path: "stats", component: TeamStatsComponent },
-  { path: "sessions", component: SessionsOverviewComponent },
-  { path: "session", component: SessionComponent }
+  { path: "sessions-overview/:id", component: SessionsOverviewComponent },
+  { path: "session/:id", component: SessionComponent }
 ];
 
 @NgModule({
