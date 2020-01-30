49fa4f3cacc4446db2b30fa72296ffbc59e072d7
Daniel S. Boniface <s1087717@monmouth.edu>
2020-01-30 18:40:45 -0500

    margin added to carousel and pitch types added to sessions-overview

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,33 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from 'rxjs';
-import {Pitcher} from '../models/Pitcher';
+import { Observable } from "rxjs";
+import { Pitcher } from "../models/Pitcher";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ApiService {
+  //apiHost = 'https://hawksbaseballpitchplus.csse-projects.monmouth.edu:3000';
+  apiHost = "https://localhost:3000";
 
-  apiHost = 'https://hawksbaseballpitchplus.csse-projects.monmouth.edu:3000';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
-   }
+  getAllPictherData(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiHost + "/pitchers");
+  }
 
-   getAllPictherData(): Observable<any[]>{
-     return this.http.get<any[]>(this.apiHost + '/pitchers');
-   }
+  getPitcherById(pitcherID): Observable<any[]> {
+    return this.http.get<any[]>(this.apiHost + "/pitchers/" + pitcherID);
+  }
 
-   getPitcherById(pitcherID): Observable<any[]> {
-    return this.http.get<any[]>(this.apiHost + '/pitchers/' + pitcherID);
-   }
-   
-   getSessionsById(pitcherId): Observable<any[]> {
-     return this.http.get<any[]>(this.apiHost + '/sessions/' + pitcherId);
-   }
+  getSessionsById(pitcherId): Observable<any[]> {
+    return this.http.get<any[]>(this.apiHost + "/sessions/" + pitcherId);
+  }
 
-   getAvgMaxByPT(sessionID, pitchType): Observable<any[]> {
-     return this.http.get<any[]>(this.apiHost + "/sessions/" + sessionID + "/" + pitchType);
-   }
-
-
+  getAvgMaxByPT(sessionID, pitchType): Observable<any[]> {
+    return this.http.get<any[]>(
+      this.apiHost + "/sessions/" + sessionID + "/" + pitchType
+    );
+  }
 }
