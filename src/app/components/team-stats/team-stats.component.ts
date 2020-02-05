f9256c9741fc08ef0594c4eaaa1ceee2545096ea
Daniel S. Boniface <s1087717@monmouth.edu>
2020-02-05 18:57:05 -0500

    switch branches

--- src/app/components/team-stats/team-stats.component.ts
+++ src/app/components/team-stats/team-stats.component.ts
@@ -24,73 +24,51 @@ export class TeamStatsComponent implements OnInit {
       "white";
   }
 
-
   allData;
 
   ngOnInit() {
-    console.log('TEAM DATA', this.tableData);
+    console.log("TEAM DATA", this.tableData);
     this.apiService.getAllPictherData().subscribe(data => {
       this.pitcherdata = data;
       console.log("Data ", this.pitcherdata);
       console.log(data.length);
-      for (var i = 0; i < data.length; i++) {
-        this.apiService.getAvgMax(data[i]._id).subscribe(data2 => {
-          console.log("data2 ", data2);
-          let maxAvg = data2;
-          this.makeTableData(i, maxAvg);
-        });
-      }
     });
-
 
     this.apiService.getAvgMax(0).subscribe(data => {
       this.allData = data;
-      console.log('ALL DATA', data);
+      console.log("ALL DATA", data);
       this.getMaxAvgs(data);
-
-
-    })
-
-    
+    });
   }
 
-<<<<<<< HEAD
-  makeTableData(id, maxAvg) {
-    // console.log(id)
-    //console.log("MAX AVG", maxAvg);
-=======
   getMaxAvgs(data) {
-   let currentID = data[0].Pitcher_pitcher_id;
+    let currentID = data[0].Pitcher_pitcher_id;
     let ids = [];
 
     let curPitches = [];
-   for(var i =0; i < data.length; i++) {
-    
-     if(currentID != data[i].Pitcher_pitcher_id) {
-       ids.push(currentID);
-       this.tableData.push(this.makeTableData(curPitches));
-       currentID = data[i].Pitcher_pitcher_id;
-       curPitches = [];
-     }
-     else {
-      curPitches.push(data[i]);
-     }
+    for (var i = 0; i < data.length; i++) {
+      if (currentID != data[i].Pitcher_pitcher_id) {
+        ids.push(currentID);
+        this.tableData.push(this.makeTableData(curPitches));
+        currentID = data[i].Pitcher_pitcher_id;
+        curPitches = [];
+      } else {
+        curPitches.push(data[i]);
+      }
 
-     if(i == data.length -1) {
-       ids.push(currentID);
-       this.tableData.push(this.makeTableData(curPitches));
-     }
-   }
+      if (i == data.length - 1) {
+        ids.push(currentID);
+        this.tableData.push(this.makeTableData(curPitches));
+      }
+    }
 
-   console.log('IDS: ', ids);
-   console.log('TABLE DATA', this.tableData);
-  //  this.tableData[currentID] = [];
-  
+    //console.log('IDS: ', ids);
+    //console.log('TABLE DATA', this.tableData);
+    // this.tableData[currentID] = [];
   }
 
   makeTableData(maxAvg) {
     console.log("MAX AVG", maxAvg);
->>>>>>> 4a41da6c06bddbcce45236cd9c4cc65a927a548c
     var pitchTypes = {};
 
     pitchTypes["4 Seam Fastball"] = [0, 0];
@@ -142,10 +120,7 @@ export class TeamStatsComponent implements OnInit {
           break;
       }
     }
-<<<<<<< HEAD
-=======
     return pitchTypes;
->>>>>>> 4a41da6c06bddbcce45236cd9c4cc65a927a548c
-    this.tableData.push(pitchTypes);
+    //this.tableData.push(pitchTypes);
   }
 }
